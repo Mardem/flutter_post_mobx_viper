373dc97258d787001c2eee39810a5adf3fb15bc4
Marden Cavalcante <main@marden.dev>
2021-11-15 16:26:55 -0300

    feat:  HttpService com interceptor para cache

---
+++ lib/services/http_service.dart
@@ -0,0 +1,107 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+import 'package:post_mobx_viper/utils/http/http_interceptor.dart';
+import 'package:post_mobx_viper/utils/http/http_mixin.dart';
+
+class HttpService with HttpMixin {
+  final String? url;
+  late final Dio _dio;
+  late final Logger _logger;
+
+  HttpService({this.url}) {
+    _setupDio();
+
+    _logger = Logger();
+  }
+
+  Future<dynamic> get({required String url}) async {
+    try {
+      late Response response;
+      response = await _dio.get(url);
+
+      return response.data;
+    } on DioError catch (e) {
+      if (e.response != null) {
+        _logger.d(e.response?.data);
+        _logger.d(e.response?.headers);
+        _logger.d(e.response?.requestOptions);
+      } else {
+        _logger.d(e.requestOptions);
+        _logger.d(e.message);
+      }
+    }
+  }
+
+  Future<dynamic> post({
+    required String url,
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      late Response response;
+      response = await _dio.post(
+        url,
+        data: data,
+        options: options,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+        onSendProgress: onSendProgress,
+      );
+
+      return response.data;
+    } on DioError catch (e) {
+      if (e.response != null) {
+        _logger.d(e.response?.data);
+        _logger.d(e.response?.headers);
+        _logger.d(e.response?.requestOptions);
+      } else {
+        _logger.d(e.requestOptions);
+        _logger.d(e.message);
+      }
+    }
+  }
+
+  Future<dynamic> put({
+    required String url,
+    data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      late Response response;
+      response = await _dio.put(
+        url,
+        data: data,
+        options: options,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+        onSendProgress: onSendProgress,
+      );
+
+      return response.data;
+    } on DioError catch (e) {
+      if (e.response != null) {
+        _logger.d(e.response?.data);
+        _logger.d(e.response?.headers);
+        _logger.d(e.response?.requestOptions);
+      } else {
+        _logger.d(e.requestOptions);
+        _logger.d(e.message);
+      }
+    }
+  }
+
+  _setupDio() {
+    _dio = Dio(BaseOptions(baseUrl: url ?? baseUrl));
+    _dio.interceptors.add(HttpCacheInterceptor());
+  }
+}
